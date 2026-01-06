4905553db36113030c353e08bc81c37d3e9fb44c
Renuka Jackson <renukajackson1@gmail.com>
2026-01-06 02:12:40 +0000

    File changes

--- src/components/inventory/InventoryAuditForm.jsx
+++ src/components/inventory/InventoryAuditForm.jsx
@@ -458,6 +458,6 @@ export default function InventoryAuditForm({ open, onOpenChange, onSuccess }) {
                     </div>
                 </DialogContent>
             </Dialog>
-        </div>
+            </div>
     );
 }
