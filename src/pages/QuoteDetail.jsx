b6d65b07ccd577331ca1064cec992a8e90241834
Renuka Jackson <renukajackson1@gmail.com>
2026-01-06 03:49:47 +0000

    File changes

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -18,7 +18,7 @@ export default function QuoteDetail() {
     const queryClient = useQueryClient();
     const [showEditDialog, setShowEditDialog] = useState(false);
 
-    const { data: allQuotes = [], isLoading: quotesLoading } = useQuery({
+    const { data: allQuotes = [], isLoading: quotesLoading, isFetching } = useQuery({
         queryKey: ['quotes'],
         queryFn: () => base44.entities.Quote.list('-created_date', 200),
         staleTime: 5 * 60 * 1000,
@@ -26,6 +26,7 @@ export default function QuoteDetail() {
     });
 
     const quote = allQuotes.find(q => q.id === quoteId);
+    const isLoadingQuote = quotesLoading || (isFetching && !quote);
 
     const { data: locations = [] } = useQuery({
         queryKey: ['clinicLocations'],
