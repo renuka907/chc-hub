6cab3bb55fbdb9969a5f3184ec599135ba9525e6
Renuka Jackson <renukajackson1@gmail.com>
2026-01-19 01:45:08 +0000

    File changes

--- src/pages/EducationDetail.jsx
+++ src/pages/EducationDetail.jsx
@@ -64,7 +64,7 @@ export default function EducationDetail() {
                 {`
                     @media print {
                         @page {
-                            margin: 0.3in 0.5in 0.5in 0.5in;
+                            margin: 0.15in 0.5in 0.5in 0.5in;
                             size: letter;
                         }
                         body {
