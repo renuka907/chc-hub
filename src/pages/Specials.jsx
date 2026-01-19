39f32e42806cc20ef1114d570ef61bbddfef3285
Renuka Jackson <renukajackson1@gmail.com>
2026-01-19 14:45:54 +0000

    File changes

--- src/pages/Specials.jsx
+++ src/pages/Specials.jsx
@@ -219,6 +219,7 @@ export default function SpecialsPage() {
                     </form>
                 </CardContent>
             </Card>
+            )}
 
             {/* Search and Specials List */}
             <div>
