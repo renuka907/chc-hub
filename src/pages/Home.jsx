986c70865b55f28690a90b4db444465911ff5d13
Renuka Jackson <renukajackson1@gmail.com>
2026-01-19 02:00:40 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -70,7 +70,7 @@ export default function Home() {
                 </div>
             </div>
 
-            <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {quickLinks.map((link) => {
                     const Icon = link.icon;
                     const gradientMap = {
