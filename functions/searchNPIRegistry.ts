e218a70ab36367c7a2efd30c60d46bbdea3b50ae
Renuka Jackson <renukajackson1@gmail.com>
2026-02-01 21:31:16 +0000

    File changes

--- functions/searchNPIRegistry.ts
+++ functions/searchNPIRegistry.ts
@@ -88,8 +88,8 @@ Deno.serve(async (req) => {
              .filter(result => {
                  // Filter to include only Florida
                  const addresses = result.addresses || [];
-                 const practiceAddress = addresses.find(a => a.address_purpose === 'LOCATION') || addresses[0] || {};
-                 if (practiceAddress.state !== 'FL') return false;
+                 const practiceAddress = addresses.find(a => a.address_purpose === 'LOCATION') || addresses[0];
+                 if (!practiceAddress || practiceAddress.state !== 'FL') return false;
 
                  // Filter to include only physicians, surgeons, and related medical providers
                  const taxonomies = result.taxonomies || [];
@@ -101,6 +101,14 @@ Deno.serve(async (req) => {
                      '207R00000X', // Osteopathic Manipulative Treatment Physicians
                      '208000000X', // Physicians
                      '2084A0401X', // Physician - Surgery - General
+                     '2084A0402X', // Physician - Surgery - Pediatric
+                     '2084A2500X', // Physician - Surgery - Cardiovascular
+                     '2084A2600X', // Physician - Surgery - Thoracic
+                     '2084N0008X', // Physician - Neurosurgery
+                     '2084P0800X', // Physician - Otolaryngology
+                     '2084F0202X', // Physician - Family Medicine
+                     '2084M0001X', // Physician - Orthopedic Surgery
+                     '2083X0100X', // Physician - Anesthesiology
                  ];
 
                  return taxonomies.some(t => 
